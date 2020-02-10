b3b9ba05caa69df007f0199291764eb6487c3efb
tthote <tthote@iu.edu>
2020-02-10 12:29:18 -0500

    CLoses: #6 [UI] Forgot password handler

--- ui/src/MainRouter.js
+++ ui/src/MainRouter.js
@@ -2,10 +2,12 @@ import React from "react";
 import {Route, Switch} from "react-router-dom";
 import Signin from "./User components/Signin";
 import Signup from "./User components/Signup";
+import home from "./User components/home"
 
 const MainRouter = () => (
     <div>
         <Switch>
+            <Route exact path="/home" component={home} />
             <Route exact path = "/signin" component={Signin} />
             <Route exact path = "/signup" component={Signup} />
         </Switch>

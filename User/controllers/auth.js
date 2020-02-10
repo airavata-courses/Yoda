2dcc5060d73fa7584bd3ba97e1c2d5820f0e29d8
tthote <tthote@iu.edu>
2020-02-10 00:00:05 -0500

    Closes: #04 [UI] Create Login component!

--- User/controllers/auth.js
+++ User/controllers/auth.js
@@ -1,5 +1,3 @@
-const jwt = require("jsonwebtoken");
-require('dotenv').config();
 const User = require("../models/user");
 
 exports.signup = async(req,res) => {
@@ -14,35 +12,5 @@ exports.signup = async(req,res) => {
     res.status(200).json({user});
 };
 
-exports.signin = (req,res) => {
-    //find user based on email
-    const {email,password} = req.body
-    User.findOne({email}, (err,user) => {
-        //if error or no user
-        if(err || !user){
-            return res.status(401).json({
-                error: "User with that email does not exist. Please sign up!"
-            })
-        }
-        //if user found, make sure the username and password match
-        if(!user.authenticate(password)){
-            return res.status(401).json({
-                error: "Email and password does not match"
-            })
-        }
-        //generate a token with user id and secret
-        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
-        //persist the token as 't' in cookie with expiry date
-        res.cookie("t", token, {expire: new Date() + 9999});
-        //return reponse with user and token to frontend client
-        const {_id, name, email } = user;
-        return res.json({token, user: {_id, email, name}}); 
-    });
-};
 
 
-exports.signout = (req, res) => {
-    //clear the cookie
-    res.clearCookie("t");
-    return res.status(200).json({ message: "Signout success!" });
-}

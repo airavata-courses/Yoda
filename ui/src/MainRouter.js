e5651e8948c634d82d69fde327c69f5f6cae03d8
Goutham Gopal <goutham.gopal94@gmail.com>
2020-02-11 17:13:57 -0500

    Updated package.json

--- ui/src/MainRouter.js
+++ ui/src/MainRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Signin from "./User components/Signin";
 import Signup from "./User components/Signup";
 import home from "./User components/home";
@@ -12,19 +12,23 @@ import activityList from "./Session components/activityList";
 import ActivityDetails from "./Session components/ActivityDetails";
 
 const MainRouter = () => (
-    <div>
-        <Switch>
-            <Route exact path="/home" component={home} />
-            <Route exact path = "/signin" component={Signin} />
-            <Route exact path = "/signup" component={Signup} />
-            <Route exact path="/forgot-password" component={ForgotPassword} />
-            <Route exact path="/reset-password/:resetPasswordToken" component={ResetPassword} />
-            <Route exact path="/signout" component={Signout} />
-            <Route exact path="/activityList" component={activityList} />
-            <Route exact path="/ActivityDetails" component={ActivityDetails} />
-            <Route exact path="/real-time" component={RealTime} />
-        </Switch>
-    </div>
+  <div>
+    <Switch>
+      <Route exact path="/" component={home} />
+      <Route exact path="/signin" component={Signin} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/forgot-password" component={ForgotPassword} />
+      <Route
+        exact
+        path="/reset-password/:resetPasswordToken"
+        component={ResetPassword}
+      />
+      <Route exact path="/signout" component={Signout} />
+      <Route exact path="/activityList" component={activityList} />
+      <Route exact path="/ActivityDetails" component={ActivityDetails} />
+      <Route exact path="/real-time" component={RealTime} />
+    </Switch>
+  </div>
 );
 
 export default MainRouter;

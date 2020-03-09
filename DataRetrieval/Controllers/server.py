540034c4044d646a3a948049e2c0c315c489716b
Goutham Gopal <goutham.gopal94@gmail.com>
2020-03-09 01:49:49 -0400

    Issue: #149 Modified travis file for DataRetrieval API

--- DataRetrieval/Controllers/server.py
+++ DataRetrieval/Controllers/server.py
@@ -10,42 +10,44 @@ from kafka import KafkaClient
 from kafka import KafkaProducer
 import pickle
 import uuid
+import os
 
 conn = nexradaws.NexradAwsInterface()
-client = KafkaClient(hosts=['localhost:9092'])
-serve = client.topic_partitions
+# client = KafkaClient(hosts=['localhost:9092'])
+# serve = client.topic_partitions
 
 app = Flask(__name__)
-created_topics = ['data-model', 'data-post', 'data-session']
-server_topics = list(serve.keys())
+# created_topics = ['data-model', 'data-post', 'data-session']
+# server_topics = list(serve.keys())
 
 topics = []
 topics.append(NewTopic(name="data-model", num_partitions=1, replication_factor=1))
 topics.append(NewTopic(name="data-post", num_partitions=1, replication_factor=1))
 topics.append(NewTopic(name="data-session", num_partitions=1, replication_factor=1))
-admin = KafkaAdminClient(bootstrap_servers=['localhost:9092'])
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+# admin = KafkaAdminClient(bootstrap_servers=['localhost:9092'])
+# producer = KafkaProducer(security_protocol='PLAINTEXT', bootstrap_servers=os.environ.get('KAFKA_HOST', 'localhost:9092'))
+producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
 
 # Check if topics already exist in kafka
-flag = 0
-val = True
-for t in created_topics:
-    flag = 0
-    for a in server_topics:
-        if t == a:
-            flag = 1
-            break
-    if flag == 0:
-        val = False
     
 def test():
     try:
+        flag = 0
+        val = True
+        for t in created_topics:
+            flag = 0
+            for a in server_topics:
+                if t == a:
+                    flag = 1
+                    break
+        if flag == 0:
+            val = False
         if not val:
             admin.delete_topics(topics=topics)
             admin.create_topics(new_topics=topics, validate_only=False)
+            print("Topics have been created in kafka")
         # return jsonify('Server Running succesfully on port 3500'), 200
     except Exception as e:
-        # return jsonify(e), 500
         print(e)
 
 '''
@@ -54,13 +56,14 @@ def test():
 '''
 @app.route('/dataretrieval/<string:radar>/<int:day>/<int:month>/<int:year>/<string:user_id>', methods=['GET', 'POST'])
 def dataRetrieve(radar, day, month, year, user_id):
-    test()
+    # test()
     try:
         if request.method == 'GET':
             # print(int(session_id))
             availData = conn.get_avail_scans(year, month, day, radar)
             print(availData)
             session_id = str(uuid.uuid4())
+            
             print(session_id)
             payload = {}
             payload['availData'] = availData[0]
@@ -71,6 +74,7 @@ def dataRetrieve(radar, day, month, year, user_id):
             payload['year'] = year
             payload['user_id'] = user_id
             pickleData = pickle.dumps(payload)
+            print(payload)
             producer.send('data-model', key=b'foo', value=pickleData)
             response = {}
             response['sessionId'] = session_id
@@ -81,4 +85,5 @@ def dataRetrieve(radar, day, month, year, user_id):
 
 if __name__ == "__main__":
     app.secret_key = 'secret'
+    # test()
     app.run(host='0.0.0.0', port=3300, debug=True)

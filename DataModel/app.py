a165c6113b7453894c6619ec1f4298c0594aedbd
Goutham Gopal <goutham.gopal94@gmail.com>
2020-03-09 01:37:56 -0400

    Closes: #147 Setup travis file for DataModel

--- DataModel/app.py
+++ DataModel/app.py
@@ -11,8 +11,8 @@ conn = nexradaws.NexradAwsInterface()
 # consumer = KafkaConsumer("data-model", auto_offset_reset='earliest', group_id='kafkagroupid', enable_auto_commit=True,
 # 						 bootstrap_servers=['localhost:9092'], api_version=(0, 10))
                          
-consumer = KafkaConsumer('data-model')
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+consumer = KafkaConsumer('data-model', bootstrap_servers=['kafka:9092'])
+producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
 
 for messages in consumer:
     print(messages.topic)

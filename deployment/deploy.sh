1b651d05f8e755dac94f103ca54a1f0f2e4e4c4b
Goutham Gopal <goutham.gopal94@gmail.com>
2020-03-21 01:19:50 -0400

    updated tags

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,14 +1,8 @@
-/bin/bash ./gatewayserver.sh
-/bin/bash ./frontendDeploy.sh
-/bin/bash ./dataretrieveDeploy.sh
-/bin/bash ./dataModelDeploy.sh
-/bin/bash ./dataPostProcessDeploy.sh
-/bin/bash ./realtimeDeploy.sh
-/bin/bash ./sessionDeploy.sh
-/bin/bash ./userDeploy.sh
-
-
-
-
-
-
+/bin/bash ./gatewayserver.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79
+/bin/bash ./frontendDeploy.sh 1847223c00f27abea897914981a16b6166b1a78f
+/bin/bash ./dataretrieveDeploy.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79
+/bin/bash ./dataModelDeploy.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79
+/bin/bash ./dataPostProcessDeploy.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79
+/bin/bash ./realtimeDeploy.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79
+/bin/bash ./sessionDeploy.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79
+/bin/bash ./userDeploy.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79

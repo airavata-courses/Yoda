21689f136612eaea60be5b69d34dbc823544d508
Goutham Gopal <goutham.gopal94@gmail.com>
2020-03-21 18:14:31 -0400

    Auto setup file change

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,4 @@
-/bin/bash ./gatewayserver.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79
+/bin/bash ./gatewayserverDeploy.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79
 /bin/bash ./frontendDeploy.sh 1847223c00f27abea897914981a16b6166b1a78f
 /bin/bash ./dataretrieveDeploy.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79
 /bin/bash ./dataModelDeploy.sh 3eba9527ad549209e741fa17b6a0e7f8ea2ecd79

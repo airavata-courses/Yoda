041a2eb5caa4d16b3e6d013d48b38949c8d5193f
Goutham Gopal <goutham.gopal94@gmail.com>
2020-04-24 21:45:27 -0400

    Updated development branch

--- DataModel/app.py
+++ DataModel/app.py
@@ -4,28 +4,38 @@ from kafka import KafkaConsumer, KafkaProducer
 import pickle
 import nexradaws
 
-conn = nexradaws.NexradAwsInterface()
+try:
+    conn = nexradaws.NexradAwsInterface()
+    consumer = KafkaConsumer('data-model', bootstrap_servers=['kafka-service:9092'], group_id="datamodel")
+    producer = KafkaProducer(bootstrap_servers=['kafka-service:9092'])
+    print("Connection established with NEXRAD and Kafka")
+except Exception as e:
+    print("Failed to connect with Nexrad API or Kafka Broker ---- exiting microservice with error -> ", str(e))
+    sys.exit()
 
-# from Controllers.utils import download_data
 
-# consumer = KafkaConsumer("data-model", auto_offset_reset='earliest', group_id='kafkagroupid', enable_auto_commit=True,
-# 						 bootstrap_servers=['localhost:9092'], api_version=(0, 10))
-                         
-consumer = KafkaConsumer('data-model', bootstrap_servers=['kafka-service:9092'], group_id="datamodel")
-producer = KafkaProducer(bootstrap_servers=['kafka-service:9092'])
-print("Connected kafka consumer")
 
-for messages in consumer:
-    print("Inside the kafka consumer for data model")
-    print("Message topic:", messages.topic)
-    print("Message key:", messages.key)
-    print("Message payload:", pickle.loads(messages.value))
-    values = pickle.loads(messages.value)
-    availData = conn.get_avail_scans(values['year'], values['month'], values['day'], values['radar'])
-    values['availData'] = availData[0]
+def on_success(metadata):
+    print("Kafka Producer ->", metadata.topic, " : success")
 
-    print('\n')
-    files = pickle.dumps(values)
-    # downloaded_data = conn.download(files, '/data')
-    producer.send('data-post', key=b'foo', value=files)
-    
+def on_error(err):
+    log.error('Producer error', exc_info=err)
+
+def model_exec():
+    for messages in consumer:
+        print("Inside the kafka consumer for data model")
+        print("Message topic:", messages.topic)
+        print("Message key:", messages.key)
+        print("Message payload:", pickle.loads(messages.value))
+        print('\n')
+        values = pickle.loads(messages.value)
+        try:
+            availData = conn.get_avail_scans(values['year'], values['month'], values['day'], values['radar'])
+            values['availData'] = availData[0]
+            files = pickle.dumps(values)
+            producer.send('data-post', key=b'foo', value=files).add_callback(on_success).add_errback(on_error)
+        except Exception as e:
+            print("error was thrown", str(e))
+
+if __name__ == "__main__":
+    model_exec()    
